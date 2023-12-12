181098b916fc162500eef0515639555405260f47
pms007 <pymyoswe007@gmail.com>
2023-12-12 20:53:42 +0700

    section6 index20

---
+++ section6/index20/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useStorage } from '@/composables/useStorage.js';
+
+let food = useStorage('food', 'pizza');
+let age = useStorage('age', 27);
+
+</script>
+
+<template>
+  <main>
+    <div>
+      <label>What is you favourite food?</label>
+      <input type="text" v-model="food" />
+    </div>
+    <div>
+      <label>What is you age?</label>
+      <input type="text" v-model="age" />
+    </div>
+  </main>
+</template>

4d5d1515b805fd8d8e34fff1a53a0fc04e454088
pms007 <pymyoswe007@gmail.com>
2023-12-15 10:55:47 +0700

    section6 index21

---
+++ section6/index21/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TabbableTextarea from '@/components/TabbableTextarea.vue';
+import { ref } from 'vue';
+
+let comment = ref('test value');
+</script>
+
+<template>
+  <main>
+    <form>
+      <TabbableTextarea v-model="comment" style="width: 100%;height: 300px;" />
+    </form>
+  </main>
+</template>

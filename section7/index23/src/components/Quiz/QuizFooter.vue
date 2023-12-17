c58db281827d3cc058a2226176b455bfe13bbd7d
pms007 <pymyoswe007@gmail.com>
2023-12-17 12:30:19 +0700

    section7 index23

---
+++ section7/index23/src/components/Quiz/QuizFooter.vue
@@ -0,0 +1,10 @@
+<script setup>
+import QuizFooterLinks from '@/components/Quiz/QuizFooterLinks.vue';
+</script>
+
+<template>
+    <footer>
+        <h4>Quiz Footer</h4>
+        <QuizFooterLinks />
+    </footer>
+</template>
---
+++ section7/index23/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Quiz from '@/components/Quiz/Quiz.vue';
+import { state } from '@/stores/quizStore.js';
+
+</script>
+
+<template>
+  <main>
+    <div class="home">
+      <Quiz :quiz="state" />
+    </div>
+  </main>
+</template>
+
+<style>
+</style>
